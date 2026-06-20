71d86815239222d8f9c852ad97a45821f807dcc7
Lynx <daffaauliad@gmail.com>
2026-06-20 15:39:43 +0800

    feat: added a basic admin-dashboard

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,39 @@
+export default function DashboardAdmin() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      
+      <header className="w-full flex justify-between bg-cyan-200 px-4 py-4 rounded-sm shadow-sm">
+        <span className="text-slate-800 text-xl font-bold">Atlas Admin Panel</span>
+        <nav className="items-end-safe flex justify-between gap-4 text-slate-800">
+          <div className="hover:text-slate-200  transition-colors duration-200">Home</div>
+        </nav>
+      </header>
+      
+      <main className="flex-1 flex items-center justify-center gap-4 px-4 py-4 text-slate-800">
+      
+        <button className="
+          bg-gray-100 rounded-xl cursor-pointer transition-all duration-150 shadow-sm w-60 py-5
+          text-center border border-transparent hover:shadow-md hover:border-indigo-300 hover:border-4 active:scale-[0.98]">
+          <h1 className="text-xl font-bold">User</h1>
+          <p>Manage User</p>
+        </button>
+      
+        <button className="
+          bg-gray-100 rounded-xl cursor-pointer transition-all duration-150 shadow-sm w-60 py-5
+          text-center border border-transparent hover:shadow-md hover:border-indigo-300 hover:border-4 active:scale-[0.98]">
+          <h1 className="text-xl font-bold">Companies</h1>
+          <p>Manage Companies</p>
+        </button>
+      
+        <button className="
+          bg-gray-100 rounded-xl cursor-pointer transition-all duration-150 shadow-sm w-60 py-5
+          text-center border border-transparent hover:shadow-md hover:border-indigo-300 hover:border-4 active:scale-[0.98]">
+          <h1 className="text-xl font-bold">Brand</h1>
+          <p>Manage Brand</p>
+        </button>
+      
+      </main>
+      
+    </div>
+  )
+}
